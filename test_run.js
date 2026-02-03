92ecc9f0c18b2846b4a208390e5cd0a34074d270
umairkhancis <umairkhancis@gmail.com>
2026-02-03 23:14:17 +0400

    Clean up old static files, keep only React app
    Removed:
    - Old static HTML files (index.html, calculator.js)
    - Outdated test files
    - Obsolete documentation
    
    The React app is now in react-app/ directory.

--- test_run.js
+++
@@ -1,6 +0,0 @@
-
-const RealEstateCalculator = require('./calculator.js');
-
-const inputs = {"propertySize": 1189, "totalValue": 1560000, "downPaymentPercent": 20, "registrationFeePercent": 4, "tenure": 25, "discountRate": 4, "rentalROI": 6, "serviceChargesPerSqFt": 10, "exitValue": 1664600};
-const results = RealEstateCalculator.calculateInvestment(inputs);
-console.log(JSON.stringify(results, null, 2));
